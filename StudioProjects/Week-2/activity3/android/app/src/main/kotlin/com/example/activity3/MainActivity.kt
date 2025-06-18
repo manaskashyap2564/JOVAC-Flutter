435005ccf9f709cc1c44df61f508903f0a1275bd
manaskashyap2564 <kashyap.manas.glams@gmail.com>
2025-06-18 23:20:03 +0530

    Jovac: W2D3-D7

---
+++ StudioProjects/Week-2/activity3/android/app/src/main/kotlin/com/example/activity3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.activity3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ StudioProjects/Week-2/assignment3/contact_page/android/app/src/main/kotlin/com/example/contact_page/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.contact_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
